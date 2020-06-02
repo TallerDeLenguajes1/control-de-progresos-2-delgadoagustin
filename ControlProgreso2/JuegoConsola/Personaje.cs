fe1bede7a82e4ed6be1eb83d96f7e9a095d34366
Agustin Delgado <delgadoagustin147@gmail.com>
2020-06-02 16:03:23 -0300

    Inicializa proyecto y crea clase personaje

---
+++ ControlProgreso2/JuegoConsola/Personaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoConsola
+{
+    enum max
+    {
+        maxVelocidad = 10,
+        maxDestreza = 5,
+        maxFuerza = 10,
+        maxNivel = 10,
+        maxArmadura = 10,
+        maxEdad = 300,
+        maxSalud = 100
+
+    }
+    class Personaje
+    {
+        int velocidad, destreza, fuerza, nivel, armadura;
+        string tipo, nombre, apodo;
+        DateTime fecNac;
+        int salud;
+    }
+}
